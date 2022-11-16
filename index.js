dfe9a6bac28397b48d1868e7036103b8369f9f7b
Hansel <you@example.com>
2022-11-16 17:35:35 -0500

    Added_404_pg

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require ('express')
+require('dotenv').config()
+const express = require('express')
 const app = express()
 
-/* first route*/
-app.get('/', (req, res) =>{
- 
-    res.send('hello world')
+app.get('/', (req, res) => {
+    res.send('Hello world!')
 })
 
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
 
-app.listen(3000)
+app.listen(process.env.PORT)
