db7a73c7282cf47c8390d6dbb5b013f480e7e699
Hansel <you@example.com>
2022-12-10 13:42:23 -0500

    delete comments

--- controllers/places.js
+++ controllers/places.js
@@ -138,69 +138,31 @@ db.Place.findById(req.params.id)
 
 // PUT ROUTES
 
+
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${req.params.id}`)
-  }
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+      .then(() => {
+          res.redirect(`/places/${req.params.id}`)
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
 })
-
-
 
 module.exports = router
 
 
+
+
+
+
+
+
+//old  routes//
+
 /*
-router.put('/:id', (req, res) => {
-res.send('PUT /places/:id stub')
-})
 
-router.delete('/:id', (req, res) => {
-res.send('DELETE /places/:id stub')
-})
-
-router.get('/:id/edit', (req, res) => {
-res.send('GET edit form stub')
-})
-
-router.post('/:id/rant', (req, res) => {
-res.send('GET /places/:id/rant stub')
-})
-
-router.delete('/:id/rant/:rantId', (req, res) => {
-  res.send('GET /places/:id/rant/:rantId stub')
-})
-
-
-
-
-
-
-
-
-
-
-//old delete route no mongoose//
 
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -233,4 +195,30 @@ router.get('/:id/edit', (req, res) => {
 })
 
 
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${req.params.id}`)
+  }
+})
 */
